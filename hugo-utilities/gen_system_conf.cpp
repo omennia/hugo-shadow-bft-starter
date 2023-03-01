5d9128da8f59c0b337507d128d9064e374c1500b
omennia <ianginthnemo@gmail.com>
2023-03-01 12:35:14 +0000

    extracting general_config

--- hugo-utilities/gen_system_conf.cpp
+++ hugo-utilities/gen_system_conf.cpp
@@ -11,8 +11,8 @@ class systema{
     public:
     string shutdownhook = "true";
     string samebatchsize = "false";
-    long long numrepliers = 16;
     string bft = "true";
+    long long numrepliers = 16;
     
     void print_c_name(){
         mf << "system.";
@@ -23,6 +23,14 @@ class systema{
         print_c_name(), mf << "samebatchsize = " << samebatchsize << '\n';
         print_c_name(), mf << "numrepliers = " << numrepliers << '\n';
         print_c_name(), mf << "bft = " << bft << '\n';
+    }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "shutdownhook") this->shutdownhook = param;
+        if(obs == "samebatchsize") this->samebatchsize = param;
+        if(obs == "bft") this->bft = param;
+        if(obs == "numrepliers") this->numrepliers = stoi(param);
     }
 };
 
@@ -57,27 +65,39 @@ class communication : public systema{
         print_sc_name(), mf << "outQueueSize = " << outQueueSize << '\n';
         print_sc_name(), mf << "useSignatures = " << useSignatures << '\n';
     }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "secretKeyAlgorithm") this->secretKeyAlgorithm = param;
+        if(obs == "hashAlgorithm") this->hashAlgorithm = param;
+        if(obs == "signatureAlgorithm") this->signatureAlgorithm = param;
+        if(obs == "defaultKeyLoader") this->defaultKeyLoader = param;
+        if(obs == "defaultkeys") this->defaultkeys = param;
+        if(obs == "inQueueSize") this->inQueueSize = stoi(param);
+        if(obs == "outQueueSize") this->outQueueSize = stoi(param);
+        if(obs == "useSignatures") this->useSignatures = stoi(param);
+    }
 };
 
 class totalordermulticast : public systema{
     public: 
     
-    long long batchtimeout = -1;
     string fairbatch = "true";
-    long long nonces = 10;
     string verifyTimestamps = "false";
     string state_transfer = "true";
-    long long highMark = 10000;
-    long long revival_highMark = 10;
-    long long timeout_highMark = 200;
     string log = "true";
     string log_parallel = "false";
     string log_to_disk = "false";
     string sync_log = "false";
-    long long checkpoint_period = 1024;
-    long long global_checkpoint_period = 120000;
     string checkpoint_to_disk = "false";
     string sync_ckp = "false";
+    long long batchtimeout = -1;
+    long long nonces = 10;
+    long long highMark = 10000;
+    long long revival_highMark = 10;
+    long long timeout_highMark = 200;
+    long long checkpoint_period = 1024;
+    long long global_checkpoint_period = 120000;
     long long maxBatchSizeInBytes = 999999999;
     long long maxbatchsize = 1000;
     long long timeout = 2000;
@@ -107,6 +127,30 @@ class totalordermulticast : public systema{
         print_sc_name(), mf << "maxbatchsize = " << maxbatchsize << '\n';
         print_sc_name(), mf << "timeout = " << timeout << '\n';
     }
+
+     void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "fairbatch") this->fairbatch = param; 
+        if(obs == "verifyTimestamps") this->verifyTimestamps = param; 
+        if(obs == "state_transfer") this->state_transfer = param; 
+        if(obs == "log") this->log = param; 
+        if(obs == "log_parallel") this->log_parallel = param; 
+        if(obs == "log_to_disk") this->log_to_disk = param; 
+        if(obs == "sync_log") this->sync_log = param; 
+        if(obs == "checkpoint_to_disk") this->checkpoint_to_disk = param; 
+        if(obs == "sync_ckp") this->sync_ckp = param; 
+        if(obs == "batchtimeout") this->batchtimeout = stoi(param);
+        if(obs == "nonces") this->nonces = stoi(param);
+        if(obs == "highMark") this->highMark = stoi(param);
+        if(obs == "revival_highMark") this->revival_highMark = stoi(param);
+        if(obs == "timeout_highMark") this->timeout_highMark = stoi(param);
+        if(obs == "checkpoint_period") this->checkpoint_period = stoi(param);
+        if(obs == "global_checkpoint_period") this->global_checkpoint_period = stoi(param);
+        if(obs == "maxBatchSizeInBytes") this->maxBatchSizeInBytes = stoi(param);
+        if(obs == "maxbatchsize") this->maxbatchsize = stoi(param);
+        if(obs == "timeout") this->timeout = stoi(param);
+
+    }
 };
 
 class ssltls : public systema{
@@ -125,6 +169,14 @@ class ssltls : public systema{
         print_sc_name(), mf << "key_store_file = " << key_store_file << '\n';
         print_sc_name(), mf << "enabled_ciphers = " << enabled_ciphers << '\n';
     }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "protocol_version") this->protocol_version = param;
+        if(obs == "key_store_file") this->key_store_file = param;
+        if(obs == "enabled_ciphers") this->enabled_ciphers = param;
+    }
+
 };
 
 class ttp : public systema{
@@ -139,6 +191,11 @@ class ttp : public systema{
     void print(){
         print_sc_name(), mf << "id = " << id << '\n';
     }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "id") this->id = stoi(param);
+    }
 };
 
 class initial : public systema{
@@ -152,6 +209,11 @@ class initial : public systema{
 
     void print(){
         print_sc_name(), mf << "view = " << view << '\n';
+    }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "view") this->view = param;
     }
 };
 
@@ -169,6 +231,12 @@ class servers : public systema{
         print_sc_name(), mf << "num = " << num << '\n';
         print_sc_name(), mf << "f = " << f << '\n';
     }
+
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "num") this->num = stoi(param);
+        if(obs == "f") this->f = stoi(param);
+    }
 };
 
 class client : public systema{
@@ -184,7 +252,73 @@ class client : public systema{
         print_sc_name(), mf << "invokeOrderedTimeout = " << invokeOrderedTimeout << '\n';
     }
 
+    void update(string obs, string param){
+        obs.pop_back(); // Remove the colon
+        if(obs == "invokeOrderedTimeout") this->invokeOrderedTimeout = stoi(param);
+    }
 };
+
+
+void parse_yaml(systema &ms, communication &com, totalordermulticast &tom, ssltls &tls, ttp &tp, initial &init, servers &ms_servers, client &ms_clients){
+    ifstream in_mf;
+
+    in_mf.open("general_config.yaml");
+
+    string cur_observer;
+    string param;
+
+    while(in_mf >> cur_observer){
+        if(cur_observer == "#"){
+            getline(in_mf, cur_observer); // ignore comments
+        }
+        else{
+            if(cur_observer == "COMMUNICATION"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                com.update(cur_observer, param);
+            }
+            else if(cur_observer == "TOTAL_ORDER_MULTICAST"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                tom.update(cur_observer, param);
+            }
+            else if(cur_observer == "SSLTLS"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                tls.update(cur_observer, param);
+            }
+            else if(cur_observer == "TTP"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                tp.update(cur_observer, param);
+            }
+            else if(cur_observer == "SYSTEM"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                ms.update(cur_observer, param);
+            }
+            else if(cur_observer == "INITIAL"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                init.update(cur_observer, param);
+            }
+            else if(cur_observer == "SERVERS"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                ms_servers.update(cur_observer, param);
+            }
+            else if(cur_observer == "CLIENT"){
+                in_mf >> cur_observer;
+                in_mf >> param;
+                ms_clients.update(cur_observer, param);
+            }
+        }
+    }
+
+    in_mf.close();
+}
+
+
 
 
 signed main(){
@@ -194,8 +328,10 @@ signed main(){
     ssltls ms_tls;
     ttp ms_ttp;
     initial ms_init;
-    client ms_clients;
     servers ms_servers;
+    client ms_clients;
+
+    parse_yaml(ms, ms_comm, ms_tom, ms_tls, ms_ttp, ms_init, ms_servers, ms_clients);
 
 
     mf.open(".syst_conf");
